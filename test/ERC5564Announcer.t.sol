1a396afa352be82750d6f87742b5f0d28de6a12d
Matt Solomon <matt@mattsolomon.dev>
2023-07-12 09:15:23 -0700

    chore: remove unused import

--- test/ERC5564Announcer.t.sol
+++ test/ERC5564Announcer.t.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 import {Deploy} from "script/Deploy.s.sol";
-import {ERC5564Announcer} from "src/ERC5564Announcer.sol";
 
 contract ERC5564AnnouncerTest is Test, Deploy {
   event Announcement(
