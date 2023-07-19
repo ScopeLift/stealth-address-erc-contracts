fe581e2a4b0f8603fa391ee80a925ccf9fd3d0be
Matt Solomon <matt@mattsolomon.dev>
2023-07-19 13:50:51 -0700

    test: add tests

--- test/ERC6538Registry.t.sol
+++ test/ERC6538Registry.t.sol
@@ -5,11 +5,52 @@ import {Test} from "forge-std/Test.sol";
 import {Deploy} from "script/Deploy.s.sol";
 
 contract ERC6538RegistryTest is Test, Deploy {
+  event StealthMetaAddressSet(
+    bytes indexed registrant, uint256 indexed schemeId, bytes stealthMetaAddress
+  );
+
   function setUp() public {
     Deploy.run();
+  }
+
+  function toBytes(address who) internal pure returns (bytes memory) {
+    return bytes.concat(bytes32(uint256(uint160(who))));
   }
 }
 
 contract RegisterKeys is ERC6538RegistryTest {
-// TODO
+  function testFuzz_EmitsStealthMetaAddressSetEvent(
+    address caller,
+    uint256 schemeId,
+    bytes memory stealthMetaAddress
+  ) external {
+    vm.prank(caller);
+    vm.expectEmit();
+    emit StealthMetaAddressSet(toBytes(caller), schemeId, stealthMetaAddress);
+    registry.registerKeys(schemeId, stealthMetaAddress);
+  }
+
+  function testFuzz_CorrectlyMapsRegistrantToSchemeIdToStealthMetaAddressInStorage(
+    address caller,
+    uint256 schemeId,
+    bytes memory stealthMetaAddress
+  ) external {
+    assertEq(registry.stealthMetaAddressOf(toBytes(caller), schemeId), "");
+    vm.prank(caller);
+    registry.registerKeys(schemeId, stealthMetaAddress);
+    assertEq(registry.stealthMetaAddressOf(toBytes(caller), schemeId), stealthMetaAddress);
+  }
+
+  // This test is a subset of `testFuzz_EmitsStealthMetaAddressSetEvent`, and is mainly present to
+  // make the `announce` method's specification more explicit. For this reason, we set the number of
+  // runs to 1 for all profiles.
+  /// forge-config: default.fuzz.runs = 1
+  /// forge-config: ci.fuzz.runs = 1
+  /// forge-config: lite.fuzz.runs = 1
+  function testFuzz_NeverReverts(address caller, uint256 schemeId, bytes memory stealthMetaAddress)
+    external
+  {
+    vm.prank(caller);
+    registry.registerKeys(schemeId, stealthMetaAddress);
+  }
 }
