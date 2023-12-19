606e51ccfb3423914f3df8a455638d7b39f94100
garyghayrat <gary.ghayrat@pm.me>
2023-12-19 16:56:03 -0500

    Add `ERC1271MockContract` and test

--- test/ERC6538Registry.t.sol
+++ test/ERC6538Registry.t.sol
@@ -72,25 +72,27 @@ contract RegisterKeysOnBehalf_Address is ERC6538RegistryTest {
     registry.registerKeysOnBehalf(alice, schemeId, signature, stealthMetaAddress);
   }
 
-  // function testFuzz_ERC1271SignatureIsValid(
-  //   string memory name,
-  //   uint256 schemeId,
-  //   bytes memory stealthMetaAddress
-  // ) external {
-  //   (address alice, uint256 alicePk) = makeAddrAndKey(name);
-  //   SigUtils.RegistrantInfo memory registrantInfo =
-  //     SigUtils.RegistrantInfo(alice, schemeId, stealthMetaAddress, 0);
-  //   bytes32 hash = sigUtils.getTypedDataHash(registrantInfo);
-  //   (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePk, hash);
-  //   bytes memory signature = abi.encodePacked(r, s, v);
+  function testFuzz_ERC1271SignatureIsValid(
+    string memory name,
+    uint256 schemeId,
+    bytes memory stealthMetaAddress
+  ) external {
+    (address alice, uint256 alicePk) = makeAddrAndKey(name);
 
-  //   vm.prank(alice);
-  //   ERC1271CompatibleContract erc1271CompatibleContract = new ERC1271CompatibleContract();
-  //   vm.expectEmit(true, true, true, true);
-  //   emit StealthMetaAddressSet(alice, schemeId, stealthMetaAddress);
-  //   vm.prank(address(erc1271CompatibleContract));
-  //   registry.registerKeysOnBehalf(alice, schemeId, signature, stealthMetaAddress);
-  // }
+    vm.prank(alice);
+    ERC1271MockContract erc1271MockContract = new ERC1271MockContract();
+    address registrant = address(erc1271MockContract);
+
+    SigUtils.RegistrantInfo memory registrantInfo =
+      SigUtils.RegistrantInfo(registrant, schemeId, stealthMetaAddress, 0 /* nonce */ );
+    bytes32 hash = sigUtils.getTypedDataHash(registrantInfo);
+    (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePk, hash);
+    bytes memory signature = abi.encodePacked(r, s, v);
+
+    vm.expectEmit(true, true, true, true);
+    emit StealthMetaAddressSet(registrant, schemeId, stealthMetaAddress);
+    registry.registerKeysOnBehalf(registrant, schemeId, signature, stealthMetaAddress);
+  }
 
   function testFuzz_UpdateStealthMetaAddress(
     string memory name,
@@ -151,104 +153,89 @@ contract RegisterKeysOnBehalf_Address is ERC6538RegistryTest {
   }
 }
 
-// contract ERC1271CompatibleContract is Test {
-//   address owner;
+contract ERC1271MockContract {
+  address public owner;
 
-//   /**
-//    * @notice Verifies that the signer is the owner of the signing contract.
-//    */
+  enum RecoverError {
+    NoError,
+    InvalidSignature,
+    InvalidSignatureLength,
+    InvalidSignatureS
+  }
 
-//   constructor() {
-//     owner = msg.sender;
-//   }
+  error ECDSAInvalidSignature();
+  error ECDSAInvalidSignatureLength(uint256 length);
+  error ECDSAInvalidSignatureS(bytes32 s);
 
-//   function isValidSignature(bytes32 _hash, bytes calldata _signature)
-//     external
-//     view
-//     returns (bytes4)
-//   {
-//     // Validate signatures
-//     if (recoverSigner(_hash, _signature) == owner) return 0x1626ba7e;
-//     else return 0xffffffff;
-//   }
+  constructor() {
+    owner = msg.sender;
+  }
 
-//   /**
-//    * @notice Recover the signer of hash, assuming it's an EOA account
-//    * @dev Only for EthSign signatures
-//    * @param _hash       Hash of message that was signed
-//    * @param _signature  Signature encoded as (bytes32 r, bytes32 s, uint8 v)
-//    */
-//   function recoverSigner(bytes32 _hash, bytes memory _signature)
-//     internal
-//     pure
-//     returns (address signer)
-//   {
-//     require(_signature.length == 65, "SignatureValidator#recoverSigner: invalid signature
-// length");
+  function isValidSignature(bytes32 hash, bytes memory signature)
+    public
+    view
+    returns (bytes4 magicValue)
+  {
+    return recover(hash, signature) == owner ? this.isValidSignature.selector : bytes4(0);
+  }
 
-//     // Variables are not scoped in Solidity.
-//     uint8 v = uint8(_signature[64]);
-//     (, bytes32 r) = readBytes32(_signature, 0);
-//     (, bytes32 s) = readBytes32(_signature, 32);
+  function recover(bytes32 hash, bytes memory signature) internal pure returns (address) {
+    (address recovered, RecoverError error, bytes32 errorArg) = tryRecover(hash, signature);
+    _throwError(error, errorArg);
+    return recovered;
+  }
 
-//     // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and
-// make
-//     // the signature
-//     // unique. Appendix F in the Ethereum Yellow paper
-//     // (https://ethereum.github.io/yellowpaper/paper.pdf), defines
-//     // the valid range for s in (281): 0 < s < secp256k1n ÷ 2 + 1, and for v in (282): v ∈ {27,
-//     // 28}. Most
-//     // signatures from current libraries generate a unique signature with an s-value in the lower
-//     // half order.
-//     //
-//     // If your library generates malleable signatures, such as s-values in the upper range,
-//     // calculate a new s-value
-//     // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v
-// from
-//     // 27 to 28 or
-//     // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27
-// to
-//     // v to accept
-//     // these malleable signatures as well.
-//     //
-//     // Source OpenZeppelin
-//     //
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/cryptography/ECDSA.sol
+  function tryRecover(bytes32 hash, bytes memory signature)
+    internal
+    pure
+    returns (address, RecoverError, bytes32)
+  {
+    if (signature.length == 65) {
+      bytes32 r;
+      bytes32 s;
+      uint8 v;
+      // ecrecover takes the signature parameters, and the only way to get them
+      // currently is to use assembly.
+      /// @solidity memory-safe-assembly
+      assembly {
+        r := mload(add(signature, 0x20))
+        s := mload(add(signature, 0x40))
+        v := byte(0, mload(add(signature, 0x60)))
+      }
+      return tryRecover(hash, v, r, s);
+    } else {
+      return (address(0), RecoverError.InvalidSignatureLength, bytes32(signature.length));
+    }
+  }
 
-//     if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
-//       revert("SignatureValidator#recoverSigner: invalid signature 's' value");
-//     }
+  function tryRecover(bytes32 hash, uint8 v, bytes32 r, bytes32 s)
+    internal
+    pure
+    returns (address, RecoverError, bytes32)
+  {
+    if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+      return (address(0), RecoverError.InvalidSignatureS, s);
+    }
 
-//     if (v != 27 && v != 28) revert("SignatureValidator#recoverSigner: invalid signature 'v'
-// value");
+    // If the signature is valid (and not malleable), return the signer address
+    address signer = ecrecover(hash, v, r, s);
+    if (signer == address(0)) return (address(0), RecoverError.InvalidSignature, bytes32(0));
 
-//     // Recover ECDSA signer
-//     signer = ecrecover(_hash, v, r, s);
+    return (signer, RecoverError.NoError, bytes32(0));
+  }
 
-//     // Prevent signer from being 0x0
-//     require(signer != address(0x0), "SignatureValidator#recoverSigner: INVALID_SIGNER");
-
-//     return signer;
-//   }
-
-//   function readBytes32(bytes memory _data, uint256 _offset)
-//     internal
-//     pure
-//     returns (uint256 newOffset, bytes32 r)
-//   {
-//     newOffset = _offset + 32;
-//     r = bytesToBytes32(_data, _offset);
-//   }
-
-//   function bytesToBytes32(bytes memory _bytes, uint256 _start) internal pure returns (bytes32 r)
-// {
-//     uint256 offset = _start + 0x20;
-//     require(_bytes.length >= offset, "Y");
-//     assembly {
-//       r := mload(add(_bytes, offset))
-//     }
-//   }
-// }
+  function _throwError(RecoverError error, bytes32 errorArg) private pure {
+    if (error == RecoverError.NoError) {
+      return; // no error: do nothing
+    } else if (error == RecoverError.InvalidSignature) {
+      revert ECDSAInvalidSignature();
+    } else if (error == RecoverError.InvalidSignatureLength) {
+      revert ECDSAInvalidSignatureLength(uint256(errorArg));
+    } else if (error == RecoverError.InvalidSignatureS) {
+      revert ECDSAInvalidSignatureS(errorArg);
+    }
+  }
+}
 
 contract SigUtils {
   bytes32 internal DOMAIN_SEPARATOR;
@@ -267,7 +254,7 @@ contract SigUtils {
     uint256 nonce;
   }
 
-  // computes the hash of a permit
+  // computes the hash
   function getStructHash(RegistrantInfo memory _info) internal pure returns (bytes32) {
     return keccak256(
       abi.encode(TYPEHASH, _info.registrant, _info.schemeId, _info.stealthMetaAddress, _info.nonce)
