0c32536de2ed267d8760f98eb70cbbcbcd2967a5
garyghayrat <gary.ghayrat@pm.me>
2024-01-30 14:57:58 -0500

    Remove 5564 interface inheritence

--- src/interfaces/IERC5564Announcer.sol
+++ src/interfaces/IERC5564Announcer.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-/// @dev Interface for calling the `ERC5564Announcer` contract, which emits an `Announcement` event
-/// to broadcast information about a transaction involving a stealth address. See
+/// @notice Interface for calling the `ERC5564Announcer` contract, which emits an `Announcement`
+/// event to broadcast information about a transaction involving a stealth address. See
 /// [ERC-5564](https://eips.ethereum.org/EIPS/eip-5564) to learn more.
 interface IERC5564Announcer {
-  /// @dev Emitted when something is sent to a stealth address.
+  /// @notice Emitted when something is sent to a stealth address.
   /// @param schemeId Identifier corresponding to the applied stealth address scheme, e.g. 1 for
   /// secp256k1, as specified in ERC-5564.
   /// @param stealthAddress The computed stealth address for the recipient.
   /// @param caller The caller of the `announce` function that emitted this event.
   /// @param ephemeralPubKey Ephemeral public key used by the sender to derive the `stealthAddress`.
   /// @param metadata Arbitrary data to emit with the event. The first byte MUST be the view tag.
-  /// The remaining metadata can be used by the senders however they like. See
+  /// @dev The remaining metadata can be used by the senders however they like. See
   /// [ERC-5564](https://eips.ethereum.org/EIPS/eip-5564) for recommendations on how to structure
   /// this metadata.
   event Announcement(
@@ -23,13 +23,13 @@ interface IERC5564Announcer {
     bytes metadata
   );
 
-  /// @dev Called by integrators to emit an `Announcement` event.
+  /// @notice Called by integrators to emit an `Announcement` event.
   /// @param schemeId Identifier corresponding to the applied stealth address scheme, e.g. 1 for
   /// secp256k1, as specified in ERC-5564.
   /// @param stealthAddress The computed stealth address for the recipient.
   /// @param ephemeralPubKey Ephemeral public key used by the sender.
   /// @param metadata Arbitrary data to emit with the event. The first byte MUST be the view tag.
-  /// The remaining metadata can be used by the senders however they like. See
+  /// @dev The remaining metadata can be used by the senders however they like. See
   /// [ERC-5564](https://eips.ethereum.org/EIPS/eip-5564) for recommendations on how to structure
   /// this metadata.
   function announce(
